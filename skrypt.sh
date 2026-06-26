14406604f9a85832a6cb1816b161ed8151b3646b
Mateusz Boron <dsw59982@student.ideis.pl>
2026-06-27 00:30:14 +0200

    dodano krotkie flagi i zaktualizowano help

--- skrypt.sh
+++ skrypt.sh
@@ -3,22 +3,24 @@ flaga=$1
 liczba=$2
 skrypt=$0
 
-if [ "$flaga" = "--date" ]; then
+if [ "$flaga" = "--date" ] || [ "$flaga" = "-d" ]; then
     date
-elif [ "$flaga" = "--logs" ]; then
+elif [ "$flaga" = "--logs" ] || [ "$flaga" = "-l" ]; then
+    # sprawdzamy czy podano liczbe
     if [ -z "$liczba" ]; then
         ile=100
     else
         ile=$liczba
     fi
 
+    # petla do tworzenia plikow
     for (( i=1; i<=ile; i++ ))
     do
         echo "plik: log${i}.txt, utworzony przez: $skrypt, data: $(date)" > "log${i}.txt"
     done
-elif [ "$flaga" = "--help" ]; then
+elif [ "$flaga" = "--help" ] || [ "$flaga" = "-h" ]; then
     echo "dostepne opcje:"
-    echo "--date : wyswietla dzisiejsza date"
-    echo "--logs [liczba] : tworzy pliki log (domyslnie 100)"
-    echo "--help : wyswietla to menu pomocy"
+    echo "--date, -d : wyswietla dzisiejsza date"
+    echo "--logs, -l [liczba] : tworzy pliki log (domyslnie 100)"
+    echo "--help, -h : wyswietla to menu pomocy"
 fi
