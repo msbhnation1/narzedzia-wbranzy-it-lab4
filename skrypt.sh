c6dfb0f67bb5c1ed65bcd829e8cabad66f0cd656
Mateusz Boron <dsw59982@student.ideis.pl>
2026-06-27 00:36:11 +0200

    dodano flage error oraz aktualizacja gitignore

--- skrypt.sh
+++ skrypt.sh
@@ -20,9 +20,23 @@ elif [ "$flaga" = "--help" ] || [ "$flaga" = "-h" ]; then
     echo "dostepne opcje:"
     echo "--date, -d : wyswietla dzisiejsza date"
     echo "--logs, -l [liczba] : tworzy pliki log (domyslnie 100)"
+    echo "--error, -e [liczba] : tworzy pliki error (domyslnie 100)"
+    echo "--init : klonuje repozytorium"
     echo "--help, -h : wyswietla to menu pomocy"
 elif [ "$flaga" = "--init" ]; then
     git clone https://github.com/msbhnation1/narzedzia-wbranzy-it-lab4.git
     export PATH=$PATH:$(pwd)/narzedzia-wbranzy-it-lab4
     echo "repozytorium sklonowane i dodane do PATH"
+elif [ "$flaga" = "--error" ] || [ "$flaga" = "-e" ]; then
+    if [ -z "$liczba" ]; then
+        ile=100
+    else
+        ile=$liczba
+    fi
+
+    for (( i=1; i<=ile; i++ ))
+    do
+        mkdir -p "error${i}"
+        echo "plik: error${i}.txt, skrypt: $skrypt, data: $(date)" > "error${i}/error${i}.txt"
+    done
 fi
