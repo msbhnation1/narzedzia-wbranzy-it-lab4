1d933c91830870df821650b4b220b5b83214fd20
Mateusz Boron <dsw59982@student.ideis.pl>
2026-06-26 22:07:57 +0200

    dodano wyswietlanie opcji help

--- skrypt.sh
+++ skrypt.sh
@@ -6,16 +6,19 @@ skrypt=$0
 if [ "$flaga" = "--date" ]; then
     date
 elif [ "$flaga" = "--logs" ]; then
-    # sprawdzamy czy podano liczbe na chlopski rozum (czy zmienna jest pusta)
     if [ -z "$liczba" ]; then
         ile=100
     else
         ile=$liczba
     fi
 
-    # petla ktora kreci sie od 1 do naszej liczby docelowej
     for (( i=1; i<=ile; i++ ))
     do
         echo "plik: log${i}.txt, utworzony przez: $skrypt, data: $(date)" > "log${i}.txt"
     done
+elif [ "$flaga" = "--help" ]; then
+    echo "dostepne opcje:"
+    echo "--date : wyswietla dzisiejsza date"
+    echo "--logs [liczba] : tworzy pliki log (domyslnie 100)"
+    echo "--help : wyswietla to menu pomocy"
 fi
